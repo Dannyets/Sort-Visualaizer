94d34fc990b4f79a33a22ae1325334812d6ca6a6
Dannyets <dannyets@gmail.com>
2019-04-10 11:43:15 +0300

    Fixed graph bounds

--- src/sort-visualizer/number-visualizer/NumberVisualizer.styles.js
+++ src/sort-visualizer/number-visualizer/NumberVisualizer.styles.js
@@ -7,6 +7,7 @@ export const NumberVisualizerContainer = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    min-width: 20px;
 `;
 
 export const VisualNumberContainer = styled.div`
--- src/sort-visualizer/numbers-visualizer/NumbersVisualizer.styles.js
+++ src/sort-visualizer/numbers-visualizer/NumbersVisualizer.styles.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 export const NumbersVisualizerContainer = styled.div`
     display: flex;
     flex-direction: row;
-    justify-content: space-evenly;
+    justify-content: end;
     align-items: flex-end;
     height: 500px;
+    overflow: -webkit-paged-x;
 `;
